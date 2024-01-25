26a50cd978a3131c23d54fd348b0be8fcaf3cb98
Adaeze Ndupu <andupu4@gmail.com>
2024-01-26 00:59:51 +0100

    set default theme

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -2,6 +2,11 @@ import { useEffect } from 'react';
 
 const useDarkMode = () => {
   useEffect(() => {
+    if (!localStorage.theme) {
+      document.documentElement.classList.add('light');
+      localStorage.theme = 'light';
+    }
+
     if (
       localStorage.theme === 'dark' ||
       (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
